3fa55b1c9ecb928e5b6af0525ea3b39b9ac36327
3bhady <mohamed_abdelhady96@hotmail.com>
2018-04-28 22:41:46 +0200

    --

--- Client/results.js
+++ Client/results.js
@@ -23,13 +23,35 @@ $.get("http://localhost:4567/search/" + localStorage.getItem("search"), function
 
 sendDelayedKeyPress = function (key) {
     console.log("first");
-    document.getElementById("searchbar").onkeypress = null;
+    document.getElementById("search-bar").onkeypress = null;
     setTimeout(function () {
         console.log("here");
         console.log(key);
-        getSearchSuggestions(document.getElementById("searchbar").value);
-        document.getElementById("searchbar").onkeypress = sendDelayedKeyPress;
+        getSearchSuggestions(document.getElementById("search-bar").value);
+        document.getElementById("search-bar").onkeypress = sendDelayedKeyPress;
     }, 1000);
 };
+getSearchSuggestions = function (text) {
+    console.log("http://localhost:4567/suggestion/" + text);
+    $.get("http://localhost:4567/suggest/" + text, function (succ) {
+        // $( ".result" ).html( data );
+        $(function () {
 
-var input = document.getElementById("searchbar");
+                var obj = JSON.parse(succ);
+                var sugg = obj.Suggestions;
+                var availableTags = sugg;
+                console.log(sugg);
+                $("#searchbar").autocomplete({
+                    source: availableTags
+                });
+            }
+        );
+        // alert( succ );
+    });
+};
+submitSearch=function(text){
+    localStorage.setItem("search", document.getElementById("search-bar").value);
+    window.location.href="results.html";
+};
+document.getElementById("search-bar").onkeypress=sendDelayedKeyPress;
+document.getElementById("mag").onclick=submitSearch;
